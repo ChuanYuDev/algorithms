2891ab89915d009373ea0bdb11b32e6faafb061a
nealmatrix <nealmatrixgg@gmail.com>
2026-06-19 00:46:19 -0700

    add Find the Highest Altitude

---
+++ Algorithm/Leetcode/FindTheHighestAltitude/MainProgram.cs
@@ -0,0 +1,59 @@
+namespace Leetcode.FindTheHighestAltitude;
+
+//  Find the Highest Altitude
+//  Easy
+//  There is a biker going on a road trip.
+//  The road trip consists of n + 1 points at different altitudes.
+//  The biker starts his trip on point 0 with altitude equal 0.
+//
+//  You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i and i + 1 for all (0 <= i < n).
+//  Return the highest altitude of a point.
+//
+//  Example 1:
+//  Input: gain = [-5,1,5,0,-7]
+//  Output: 1
+//  Explanation: The altitudes are [0,-5,-4,1,1,-6].
+//
+//  Example 2:
+//  Input: gain = [-4,-3,-2,-1,4,3,2]
+//  Output: 0
+//  Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+//
+//  Constraints:
+//  n == gain.length
+//  1 <= n <= 100
+//  -100 <= gain[i] <= 100
+
+//  Time complexity: O(n)
+//  Space complexity: O(1)
+
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        var largestAltitude = 0;
+        var altitude = 0;
+        foreach (var g in gain)
+        {
+            altitude += g;
+            largestAltitude = Math.Max(largestAltitude, altitude);
+        }
+
+        return largestAltitude;
+    }
+}
+
+public class MainProgram
+{
+    static void Main()
+    {
+        var sol = new Solution();
+        int[] gain = [-5, 1, 5, 0, -7];
+        Console.WriteLine(sol.LargestAltitude(gain));
+        //  Output: 1
+
+        gain = [-4, -3, -2, -1, 4, 3, 2];
+        Console.WriteLine(sol.LargestAltitude(gain));
+        //  Output: 0
+    }
+}
