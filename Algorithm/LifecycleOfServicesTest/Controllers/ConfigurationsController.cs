b6298ba8a60c90da222ee500f45470ca9196455d
nealmatrix <nealmatrixgg@gmail.com>
2026-07-08 20:46:44 -0700

    add LifecycleOfServicesTest project

---
+++ Algorithm/LifecycleOfServicesTest/Controllers/ConfigurationsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifecycleOfServicesTest.Controllers;
+
+[Route("api/configurations")]
+public class ConfigurationsController: ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public ConfigurationsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("get-connection-string")]
+    public IActionResult GetConnectionString()
+    {
+        var connectionString = _configuration.GetValue<string>("myConnectionString");
+
+        return Ok(connectionString);
+    }
+}
